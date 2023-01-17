edd36b7573aee5394aa668af9f4a9b863231180d
VasylDanylevych <ukrainec31@gmail.com>
2023-01-17 21:03:09 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
 
+const onPlay = function({seconds}) {
+localStorage.setItem("videoplayer-current-time", seconds)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle'
 
+const form = document.querySelector(".feedback-form");
+const message = document.querySelector(".feedback-form message");
+const email = document.querySelector(".feedback-form email");
+
+let formElements = {};
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+
+populateForm()
+
+function onFormInput(evt) {
+    // formElements.email = evt.target.elements.email.value;
+    // formElements.message = evt.target.elements.message.value;
+    formElements[evt.target.name] = evt.target.value;
+    const formValueJSON = JSON.stringify(formElements);
+    localStorage.setItem(STORAGE_KEY, formValueJSON)
+};
+
+function populateForm() {
+    const savedFormElements = localStorage.getItem(STORAGE_KEY);
+    const parsedFormElements = JSON.parse(savedFormElements);
+    if (parsedFormElements) {
+        formElements.email = parsedFormElements.email;
+        formElements.message = parsedFormElements.message;
+        form.elements.email.value = formElements.email;
+        form.elements.message.value = formElements.message;
+    };
+};
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    console.log(formElements);
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
